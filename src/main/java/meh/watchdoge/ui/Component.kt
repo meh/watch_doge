fd0c59b1db4a34bf67bab9391ab0d092bef95410
meh <meh@schizofreni.co>
2016-02-17 03:14:00 +0100

    ui.Component: make it a little more flexible

--- src/main/java/meh/watchdoge/ui/Component.kt
+++ src/main/java/meh/watchdoge/ui/Component.kt
@@ -1,21 +1,39 @@
 package meh.watchdoge.ui;
 
 import android.content.Context;
+import android.content.ContextWrapper;
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
 import android.view.View;
 
-open class Component(context: Context, layout: Int) {
-	protected val view: View;
+open class Component: ContextWrapper {
+	protected val _view: View;
 
-	init {
-		view = LayoutInflater.from(context).inflate(layout, null);
+	constructor(context: Context, view: View) : super(context) {
+		_view = view;
+		prepare();
+	}
+
+	constructor(context: Context, layout: Int) : super(context) {
+		_view = LayoutInflater.from(context).inflate(layout, null)
 		prepare();
 	}
 
 	open fun prepare() { }
 
-	fun addTo(group: ViewGroup) {
-		group.addView(view);
+	fun view(): View {
+		return _view;
+	}
+
+	fun addTo(group: ViewGroup, index: Int) {
+		group.addView(_view, index);
+	}
+
+	fun appendTo(group: ViewGroup) {
+		group.addView(_view);
+	}
+
+	fun prependTo(group: ViewGroup) {
+		group.addView(_view, 0);
 	}
 }
